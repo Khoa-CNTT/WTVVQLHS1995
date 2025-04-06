03a8f5be78732441b814aed89ea654042c529bdf
thjnh-t <phailacoder2003@gmail.com>
2025-04-06 23:18:29 +0700

    db

--- LegAI/Backend/src/config/database.js
+++ LegAI/Backend/src/config/database.js
@@ -2,19 +2,19 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 // Kiểm tra các biến môi trường
-const requiredEnvVars = ['DB_HOST', 'DB_USER', 'DB_PASSWORD', 'DB_NAME', 'DB_PORT'];
-requiredEnvVars.forEach((varName) => {
-    if (!process.env[varName]) {
-        throw new Error(`Thiếu biến môi trường: ${varName}`);
-    }
-});
+// const requiredEnvVars = ['DB_HOST', 'DB_USER', 'DB_PASSWORD', 'DB_NAME', 'DB_PORT'];
+// requiredEnvVars.forEach((varName) => {
+//     if (!process.env[varName]) {
+//         throw new Error(`Thiếu biến môi trường: ${varName}`);
+//     }
+// });
 
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+    host: 'localhost',
+    user: 'postgres',
+    password: '123456',
+    database: 'legai',
+    port: 5432,
 });
 
 pool.connect((err, client, release) => {
